4ae250d21e252ee03813a7671518ef317c012a9b
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-04 16:18:38 +0100

    sistemo settimeout

--- script/script.js
+++ script/script.js
@@ -6,9 +6,30 @@ Da lì parte un timer di 30 secondi. (Cioè lasciate i numeri visibili per 30 se
 Dopo aver nascosto i numeri chiedete all'utente (con dei prompt) di inserirli in ordine, uno alla volta.
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
 
-PSEUDOCODIFICA
--CREA UNA ARRAY CON 5 NUMERI CASUALI ERENDILI VISIBILI
+/*PSEUDOCODIFICA
+-CREA UNA ARRAY CON 5 NUMERI CASUALI E RENDILI VISIBILI
 -CON UN SETTIMEOUT FALLI SCOMPARIRE DOPO 30 SECONDI (CONSOLE LOG VUOTO)
 -IN UNA FOR CON 5 ITERAZIONI CHIEDO ALL'UTENTE QUALI FOSSERO I NUMERI IN ORDINE.
     -SE UN NUMERO INSERITO NON CORRISPONDEVA INCREMENTA IL CONTATORE DEI NUMERI SBAGLIATI
--ALLA FINE DELLA FOR STAMPA IL CONTATORE NUMERI SBAGLIATI E DIGLI QUALI SONO QUELLI GIUSTI
+-ALLA FINE DELLA FOR STAMPA IL CONTATORE NUMERI SBAGLIATI E DIGLI QUALI SONO QUELLI GIUSTI*/
+
+let arrRandomNum = [];
+let randomNum
+for (i=0; i<5; i++){
+    // let randomNum;
+    do {
+        randomNum = (Math.floor(Math.random() * (100 - 1)));
+    } while (arrRandomNum.includes(randomNum));
+    arrRandomNum.push(Math.floor(Math.random() * (100 - 1)));
+    // alert(arrRandomNum);
+}
+console.log(arrRandomNum);
+
+
+setTimeout(emptyScreenFunction, 2000);
+
+function emptyScreenFunction(){
+    // alert(arrRandomNum);
+
+    console.log('');
+}
