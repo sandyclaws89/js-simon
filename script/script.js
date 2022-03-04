274c741f15df05ef60fd219f63cf2584039b2946
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-04 15:58:30 +0100

    pseudocodifica da ottimizzare

--- script/script.js
+++ script/script.js
@@ -6,4 +6,9 @@ Da lì parte un timer di 30 secondi. (Cioè lasciate i numeri visibili per 30 se
 Dopo aver nascosto i numeri chiedete all'utente (con dei prompt) di inserirli in ordine, uno alla volta.
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
 
-PS
+PSEUDOCODIFICA
+-CREA UNA ARRAY CON 5 NUMERI CASUALI ERENDILI VISIBILI
+-CON UN SETTIMEOUT FALLI SCOMPARIRE DOPO 30 SECONDI (CONSOLE LOG VUOTO)
+-IN UNA FOR CON 5 ITERAZIONI CHIEDO ALL'UTENTE QUALI FOSSERO I NUMERI IN ORDINE.
+    -SE UN NUMERO INSERITO NON CORRISPONDEVA INCREMENTA IL CONTATORE DEI NUMERI SBAGLIATI
+-ALLA FINE DELLA FOR STAMPA IL CONTATORE NUMERI SBAGLIATI E DIGLI QUALI SONO QUELLI GIUSTI
