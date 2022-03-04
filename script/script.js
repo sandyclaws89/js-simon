7810c0951108d53e9c65359c7547313f1b61339b
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-04 15:58:00 +0100

    Impostazione pseudocodifica (la scriveròm  meglio step by step)

---
+++ script/script.js
@@ -0,0 +1,9 @@
+
+
+/*Descrizione:
+Visualizzare in pagina 5 numeri casuali.
+Da lì parte un timer di 30 secondi. (Cioè lasciate i numeri visibili per 30 secondi allo scadere dei quali li nascondete)
+Dopo aver nascosto i numeri chiedete all'utente (con dei prompt) di inserirli in ordine, uno alla volta.
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
+
+PS
